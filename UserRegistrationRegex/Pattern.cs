7bd1f488a24f177b2843f5bc7c4cabbc5d770d69
aayusharya1 <aayusharya07@gmail.com>
2020-10-07 17:04:00 +0530

    [Aayush] Add. Code for validating firstname

---
+++ UserRegistrationRegex/Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationRegex
+{
+    class Pattern
+    {
+        string Regex_fName = "^[A-Z][a-z]{2,}$";
+
+        public bool ValidateFname(string fname)
+        {
+            return Regex.IsMatch(fname, Regex_fName);
+        }
+
+
+    }
+}
---
+++ UserRegistrationRegex/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserRegistrationRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            bool result = pattern.ValidateFname("Aayush12");
+            Console.WriteLine(result);
+        }
+    }
+}
