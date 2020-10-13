005fb99fad239b2edf6138e82de5e7fcfed4f515
aayusharya1 <aayusharya07@gmail.com>
2020-10-14 01:16:53 +0530

    [Aayush] Add. Code for adding parametrized test cases

--- UserRegistrationRegexTest/UnitTest1.cs
+++ UserRegistrationRegexTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
 using System.Xml.Schema;
 using UserRegistrationRegex;
 
@@ -8,10 +9,12 @@ namespace UserRegistrationRegexTest
     public class UnitTest1
     {
         [TestMethod]
-        public void Test_Valid_First_Name()
+
+        [DataRow("Aayush")]
+        [DataRow("AAYUSH")]
+        public void Test_Valid_First_Name(string fname)
         {
             // Arrange
-            string fname = "Aayush";
             bool expected = true;
             Pattern pattern = new Pattern();
 
@@ -21,11 +24,33 @@ namespace UserRegistrationRegexTest
             //Assert
             Assert.AreEqual(actual, expected);
         }
+
+
         [TestMethod]
-        public void Test_Valid_Last_Name()
+
+        [DataRow("aayush")]
+        [DataRow("Ay")]
+        [DataRow("Aayush Arya")]
+        public void Test_Invalid_First_Name(string fname)
         {
             // Arrange
-            string Lname = "Arya";
+            bool expected = false;
+            Pattern pattern = new Pattern();
+
+            //Act
+            bool actual = pattern.ValidateName(fname);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        [DataRow("Arya")]
+        [DataRow("Aryaaa")]
+        public void Test_Valid_Last_Name(string Lname)
+        {
+            
+            // Arrange
             bool expected = true;
             Pattern pattern = new Pattern();
 
@@ -35,11 +60,33 @@ namespace UserRegistrationRegexTest
             //Assert
             Assert.AreEqual(actual, expected);
         }
+
         [TestMethod]
-        public void Test_Valid_Email()
+        [DataRow("arya")]
+        [DataRow("AAryaaa")]
+        public void Test_Invalid_Last_Name(string Lname)
+        {
+
+            // Arrange
+            bool expected = false;
+            Pattern pattern = new Pattern();
+
+            //Act
+            bool actual = pattern.ValidateName(Lname);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+
+        [DataRow("aayush@gmail.com")]
+        [DataRow("aayush.cg@gmail.co.in")]
+        [DataRow("aaay@gmail.co.in")]
+        [DataRow("aayush01@gmail.com")]
+        public void Test_Valid_Email(string email)
         {
             // Arrange
-            string email = "Aayush.arya@gmail.com";
             bool expected = true;
             Pattern pattern = new Pattern();
 
@@ -49,25 +96,79 @@ namespace UserRegistrationRegexTest
             //Assert
             Assert.AreEqual(actual, expected);
         }
+
         [TestMethod]
-        public void Test_Valid_Mobile_Number()
+
+        [DataRow("aayush.@gmail.com")]
+        [DataRow("aaay@gmail.co.india")]
+        [DataRow("aayush01@gmail.com.12")]
+        public void Test_Invalid_Email(string email)
         {
             // Arrange
-            string MobNo = "91 9999888877";
-            bool expected = true;
+            bool expected = false;
             Pattern pattern = new Pattern();
 
             //Act
-            bool actual = pattern.ValidateMobileNo(MobNo);
+            bool actual = pattern.ValidateEmail(email);
 
             //Assert
             Assert.AreEqual(actual, expected);
         }
+
         [TestMethod]
-        public void Test_Valid_Password()
+
+        [DataRow("91 9811111111")]
+       public void Test_Valid_Mobile_Number(string mobNo)
         {
             // Arrange
-            string password = "aaYu12sh@.aaa";
+            bool expected = true;
+            Pattern pattern = new Pattern();
+
+            //Act
+            bool actual = pattern.ValidateMobileNo(mobNo);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+
+        [DataRow("919811111111")]
+        public void Test_Invalid_Mobile_Number(string mobNo)
+        {
+            // Arrange
+            bool expected = false;
+            Pattern pattern = new Pattern();
+
+            //Act
+            bool actual = pattern.ValidateMobileNo(mobNo);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+
+        [DataRow("aaYu12sh@.aaa")]
+        public void Test_Valid_Password(string password)
+        {
+            // Arrange
+            bool expected = true;
+            Pattern pattern = new Pattern();
+
+            //Act
+            bool actual = pattern.ValidatePassword(password);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        [DataRow("aayu12sh@.aaa")]
+        [DataRow("aayusharya")]
+        public void Test_Invalid_Password(string password)
+        {
+            // Arrange
             bool expected = false;
             Pattern pattern = new Pattern();
 
